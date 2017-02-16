ddc5eb20cb54c1e2de32f34e782804c8393503a2
Michael van Vliet <info@michaelvanvliet.nl>
2017-02-16 13:46:06 +0100

    Making the container compatible with the new PhenoMeNal setup

--- runTest1.sh
+++ runTest1.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 
 # run example datasets
-php -f src/vfetc.php files=data/vendor/agilent/example_batch1.txt,data/vendor/agilent/example_batch2.txt outputfile=/tmp/agilent.txt
-php -f src/vfetc.php files=data/vendor/sciex/example_batch1.txt outputfile=/tmp/sciex.txt
-php -f src/vfetc.php files=data/vendor/shimadzu/example_batch1.txt,data/vendor/shimadzu/example_batch2.txt,data/vendor/shimadzu/example_batch3.txt outputfile=/tmp/shimadzu.txt
-php -f src/vfetc.php files=data/vendor/waters/example_batch1.txt,data/vendor/waters/example_batch2.txt outputfile=/tmp/waters.txt
+php -f /files/ms-vfetc/src/vfetc.php files=/files/ms-vfetc/data/vendor/sciex/example_batch1.txt outputfile=/tmp/sciex.txt
+php -f /files/ms-vfetc/src/vfetc.php files=/files/ms-vfetc/data/vendor/agilent/example_batch1.txt,/files/ms-vfetc/data/vendor/agilent/example_batch2.txt outputfile=/tmp/agilent.txt
+php -f /files/ms-vfetc/src/vfetc.php files=/files/ms-vfetc/data/vendor/shimadzu/example_batch1.txt,/files/ms-vfetc/data/vendor/shimadzu/example_batch2.txt,/files/ms-vfetc/data/vendor/shimadzu/example_batch3.txt outputfile=/tmp/shimadzu.txt
+php -f /files/ms-vfetc/src/vfetc.php files=/files/ms-vfetc/data/vendor/waters/example_batch1.txt,/files/ms-vfetc/data/vendor/waters/example_batch2.txt outputfile=/tmp/waters.txt
 
 # compare output
-if ! cmp -s "/ms-vfetc/data/tmp/agilent.txt" "/tmp/agilent.txt"; then
+if ! cmp -s "/files/ms-vfetc/data/tmp/agilent.txt" "/tmp/agilent.txt"; then
 	echo "Agilent files don't match!"
 	exit 1
 fi
 
-if ! cmp -s "/ms-vfetc/data/tmp/sciex.txt" "/tmp/sciex.txt"; then
+if ! cmp -s "/files/ms-vfetc/data/tmp/sciex.txt" "/tmp/sciex.txt"; then
 	echo "Sciex files don't match!"
 	exit 1
 fi
 
-if ! cmp -s "/ms-vfetc/data/tmp/shimadzu.txt" "/tmp/shimadzu.txt"; then
+if ! cmp -s "/files/ms-vfetc/data/tmp/shimadzu.txt" "/tmp/shimadzu.txt"; then
 	echo "Shimadzu files don't match!"
 	exit 1
 fi
 
-if ! cmp -s "/ms-vfetc/data/tmp/waters.txt" "/tmp/waters.txt"; then
+if ! cmp -s "/files/ms-vfetc/data/tmp/waters.txt" "/tmp/waters.txt"; then
 	echo "Waters files don't match!"
 	exit 1
 fi
