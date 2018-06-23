77f2425d6eac6d893c84939e6e085e1597cdecea
Michael van Vliet <info@michaelvanvliet.nl>
2018-06-23 21:31:54 +0200

    Enable use of zip files

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,8 @@ MAINTAINER PhenoMeNal-H2020 Project ( phenomenal-h2020-users@googlegroups.com )
 ENV TOOL_NAME="ms-vfetc" \
     TOOL_VERSION=1.0 \
     CONTAINER_VERSION=1.5 \
-    TOOL_DOWNLOAD_URL="https://github.com/leidenuniv-lacdr-abs/ms-vfetc/archive/v0.5.tar.gz" \
+    # TOOL_DOWNLOAD_URL="https://github.com/leidenuniv-lacdr-abs/ms-vfetc/archive/v0.5.tar.gz" \
+    TOOL_DOWNLOAD_URL="https://github.com/leidenuniv-lacdr-abs/ms-vfetc/archive/master.tar.gz" \
     TOOL_TAR_GZ="ms-vfetc.tar.gz"
 
 LABEL software="ms-vfetc"
@@ -17,6 +18,8 @@ ADD runTest1.sh /files/${TOOL_NAME}/runTest1.sh
 # install and prepare ms-vfetc, when done run the tests
 RUN apk update && \
     apk --no-cache add curl bash && \
+    apk add --update --no-cache libintl icu icu-dev libxml2-dev && \
+    docker-php-ext-install intl zip soap && \
     cd /files/${TOOL_NAME} && \
     curl -L --insecure -o $TOOL_TAR_GZ $TOOL_DOWNLOAD_URL && \
     tar -xvzf $TOOL_TAR_GZ --strip-components 1 && \
--- rebuild_and_run.sh
+++ rebuild_and_run.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
-docker build --no-cache -t ms-vfetc . && docker run -it --rm -v $(pwd)/data/tmp:/out ms-vfetc files=/files/ms-vfetc/data/vendor/agilent/example_batch1.txt,/files/ms-vfetc/data/vendor/agilent/example_batch2.txt outputfile=/out/agilent.txt && tail data/tmp/agilent.txt
-docker build --no-cache -t ms-vfetc . && docker run -it --rm -v $(pwd)/data/tmp:/out ms-vfetc files=/files/ms-vfetc/data/vendor/sciex/example_batch1.txt outputfile=/out/sciex.txt && tail data/tmp/sciex.txt
-docker build --no-cache -t ms-vfetc . && docker run -it --rm -v $(pwd)/data/tmp:/out ms-vfetc files=/files/ms-vfetc/data/vendor/shimadzu/example_batch1.txt,/files/ms-vfetc/data/vendor/shimadzu/example_batch2.txt,/files/ms-vfetc/data/vendor/shimadzu/example_batch3.txt outputfile=/out/shimadzu.txt && tail data/tmp/shimadzu.txt
-docker build --no-cache -t ms-vfetc . && docker run -it --rm -v $(pwd)/data/tmp:/out ms-vfetc files=/files/ms-vfetc/data/vendor/waters/example_batch1.txt,/files/ms-vfetc/data/vendor/waters/example_batch2.txt outputfile=/out/waters.txt && tail data/tmp/waters.txt
+docker build --no-cache -t ms-vfetc .
+docker run -it --rm -v $(pwd)/data:/data ms-vfetc files=/data/vendor/agilent/batches.zip outputfile=/data/agilent_from_zip.txt && tail data/agilent_from_zip.txt
+docker run -it --rm -v $(pwd)/data:/data ms-vfetc files=/data/vendor/agilent/example_batch1.txt,/data/vendor/agilent/example_batch2.txt outputfile=/data/agilent.txt && tail data/agilent.txt
+docker run -it --rm -v $(pwd)/data:/data ms-vfetc files=/data/vendor/sciex/example_batch1.txt outputfile=/data/sciex.txt && tail data/sciex.txt
+docker run -it --rm -v $(pwd)/data:/data ms-vfetc files=/data/vendor/shimadzu/example_batch1.txt,/data/vendor/shimadzu/example_batch2.txt,/data/vendor/shimadzu/example_batch3.txt outputfile=/data/shimadzu.txt && tail data/shimadzu.txt
+docker run -it --rm -v $(pwd)/data:/data ms-vfetc files=/data/vendor/waters/example_batch1.txt,/data/vendor/waters/example_batch2.txt outputfile=/data/waters.txt && tail data/waters.txt
